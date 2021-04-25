8cc559727b19b8401a27847d9f23914499862f9a
esnue <lena_wagner@outlook.de>
2021-04-25 18:23:13 +0200

    4 prof cards + general code structure

--- 5_topics_app.py
+++ 5_topics_app.py
@@ -2,93 +2,119 @@
 This module contains the app for Streamlit. 
 """
 
-import streamlit as st
 import numpy as np
 import os
 import pandas as pd
+import streamlit as st
 import streamlit.components.v1 as components
-
 
 # =============================================================================
 # Header and Options
 # =============================================================================
 
-st.set_page_config(layout="centered", initial_sidebar_state="auto", page_title="Hertie Thesis Allocation") 
-st.set_option('deprecation.showPyplotGlobalUse', False) # suppress depreciation warning
-
-#st.title("Hertie School: Supervisors' Areas of Expertise")
+st.set_page_config(layout="centered", initial_sidebar_state="auto", page_title="Hertie Supervisor Tool") 
 
 # =============================================================================
-# Side Bar
+# About Page
 # =============================================================================
+
 def about():
-
-	st.sidebar.subheader("About")
-    st.sidebar.markdown("This app is created by Lena Wagner and Ba Linh Le for the course Natural Language Process with Deep Learning at Hertie School taught by Prof. Dr. Slava Jankin.")
-
-    st.sidebar.subheader("What is this?")
-    st.sidebar.markdown("This is an app designed to help you match your research proposal with possible thesis supervisors.")
-
-    #st.sidebar.subheader("What should I do?")
-    #st.sidebar.markdown("1. Copy and paste your research proposal in the box. You can also just type your research interests. \n2. A table of professors will pop up in decreasing order of suitability. You can hover over their names to see keywords used by them in their papers.\n3. Click on any professor to see their specific supervision plan.")
-
-    st.sidebar.subheader("How does this work?")
-    st.sidebar.markdown("We used a Deep Learning topic model called [Contexualized Topic Model](https://github.com/MilaNLProc/contextualized-topic-models) to generate topic proportions for each professor based on their published academic papers. \n\nThe papers we sourced from Google Scholar, so that you don't have to go through the tedious work of reading their papers to get a feeling for the match. \n\nThe supervision plans are also available at [MyStudies](https://mystudies.hertie-school.org/en/).")
-
-    st.sidebar.subheader("And then?")
-    #st.sidebar.markdown("You're free to take up the recommendations or to ignore them. :) \n\nWe strongly encourage you use it even if you have a strong preference for a professor already because who knows? Maybe there's a better fit out there! \n\nWe hope that we can help you making your choices for supervisors and wish you the best of luck with your thesis!")
-    st.sidebar.markdown("You can use the visualizations to get an overview of the topics represented by each supervisor.")
-
-    st.sidebar.subheader("Credits")
-    st.sidebar.markdown("This project would not have been possible without the ground work done by our [fellow students](https://github.com/cbsobral/python).") 
+	st.title("About this Tool")
+	st.write('''This is an app designed to help you match your research proposal with possible thesis supervisors by giving you a quick and easy overview over the wide range of academic expertise of supervisors at Hertie.
+	We hope to spare you the tedious work of going through each supervision plan, but that you are rather able to narrow down your options. 
+	It was created by Lena Wagner and Ba Linh Le for the course Natural Language Processing with Deep Learning taught by Prof. Dr. Slava Jankin.''')
+	st.title("How does this work?")
+	st.write('''We used a Deep Learning topic model called [Contexualized Topic Model](https://github.com/MilaNLProc/contextualized-topic-models) to generate topic proportions for each professor based on the body of 
+	publicy available academic papers. The papers were sourced from Google Scholar. 
+	The supervision plans are available at [MyStudies](https://mystudies.hertie-school.org/en/).''')
+	st.title("And now?")
+	st.write('''
+	You can use the different visualizations to get an overview of the topics that were generated from each supervisor's body of research.
+	Hopefully, this will make it easier for you to find a thesis supervisor who is suitable for your research proposal.
+	''')
 
 # =============================================================================
-# Load Data and Models
+# Main Page
 # =============================================================================
 
-topics_df_wide = pd.read_csv('data/train-label_wide.csv')
-topics_df_long = pd.read_csv('data/train-label_long.csv')
-
-# =============================================================================
-# Supervisors Image Buttons
-# =============================================================================
-supervisors = ['Anheier', 'Bryson', 'Centre for International Security', 'Cali',
-              'Cingolani', 'Costello', 'Flachsland', 'Graf', 'Hallerberg',
-              'Hammerschmid', 'Hassel', 'Hirth', 'Hustedt','Iacovone',
-              'Jachtenfuchs','Jankin', 'Kayser','Kreyenfeld','Mair','Mena',              
-              'Mungiu-Pippidi', 'Munzert','Patz','Reh','Roemmele','Shaikh',
-              'Snower','Stockmann','Traxler', 'Wegrich']
+def main():
 
 	# if you want to use your own styling, uncomment the line below and edit a css file
     # local_css("style.css")
-def main():
 
-	#st.image("thesis.png", width=600)
+	st.image("thesis.png", width=600)
 	activities = ["Home", "About"]
-	choice = st.sidebar.selectbox("Pick something fun", activities)
+	choice = st.sidebar.selectbox("Navigation", activities)
 	if choice == "Home":
-		st.title("Master Thesis Supervisor Recommendation")
-		st.write("""Here are your  
+		#st.title("Master Thesis Supervisor Recommendation")
+		st.write("""
+			This is a tool to recommend suitable thesis supervisor for Hertie School students.
+			
+			Here are your professors: 
 			""")
 		components.html(
 		    """
 		    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-			<div class="card" style="width: 18rem;">
-			  <img class="card-img-top" src="https://sjankin.com/images/profile.jpg" alt="Slava Jankin">
-			  <div class="card-body">
-			    <h5 class="card-title">Slava Jankin</h5>
-			    <p class="card-text">Slava Jankin is Professor of Data Science and Public Policy at the Hertie School. He is the Director of the Hertie School Data Science Lab. His research and teaching is primarily in the field of natural language processing and machine learning.</p>
-			    <a href="#" class="btn btn-primary">See more</a>
-			  </div>
+			
+		<div class="row">
+		<div class="col-sm-6">
+			<div class="card" id="jankin" style="width: 18rem;">
+				<img class="card-img-top" src="https://sjankin.github.io/images/profile.jpg" alt="Slava Jankin">
+				<div class="card-body">
+					<h5 class="card-title">Slava Jankin</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Professor of Data Science and Public Policy</h6>
+					<a href="#" class="btn btn-primary">See more</a>
+				</div>
 			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="card" id="anheier" style="width: 18rem;">
+				<img class="card-img-top" src="https://hertieschool-f4e6.kxcdn.com/fileadmin/5_WhoWeAre/1_People_directory/Faculty_downloads/Anheier/HelmutKAnheier_Copyright_HertieSchool.jpg" alt="Helmut Anheier">
+				<div class="card-body">
+					<h5 class="card-title">Helmut K. Anheier</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Professor of Sociology</h6>
+					<a href="#" class="btn btn-primary">See more</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="card" id="bryson" style="width: 18rem;">
+				<img class="card-img-top" src="https://hertieschool-f4e6.kxcdn.com/fileadmin/user_upload/Joanna_Bryson_800px.jpg" alt="Joanna Bryson">
+				<div class="card-body">
+					<h5 class="card-title">Joanna Bryson</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Professor of Ethics and Technology</h6>
+					<a href="#" class="btn btn-primary">See more</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div class="card" id="graf" style="width: 18rem;">
+				<img class="card-img-top" src="https://hertieschool-f4e6.kxcdn.com/fileadmin/user_upload/Lukas_Graf_Copyright_HertieSchoolofGovernance.jpg" alt="Lukas Graf">
+				<div class="card-body">
+					<h5 class="card-title">Lukas Graf</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Assistant Professor of Educational Governance</h6>
+					<a href="#" class="btn btn-primary">See more</a>
+				</div>
+			</div>
+		</div>
+
+		</div>
+
 		    """,
 		    height=620,
 		)
 	elif choice == "About":
 		about()
+
+
+
+if __name__ == "__main__":
+    main()
 
 # =============================================================================
 # Visualization Topics
@@ -114,10 +140,3 @@ def main():
 #else:
 
 #st.bar_chart(topics_df_wide)
-
-# =============================================================================
-# Call functions
-# =============================================================================
-
-if __name__ == "__main__":
-    main()
